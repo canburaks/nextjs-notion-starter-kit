8e2f0230afbfd9023e7feeb7b7181c2726544bc2
Can Burak S <canburak@msn.com>
2021-09-24 12:54:17 +0300

    ...

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,9 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 {config.author}</div>
+      <div className={styles.copyright}>
+        Copyright 2021. <a href="https://www.cbsofyalioglu.com/" rel="me">{config.author}</a>
+      </div>
 
       {hasMounted ? (
         <div className={styles.settings}>
