d5ee548aa38949e2b4664a7b5191bf648b39ebe0
Can Burak S <canburak@msn.com>
2021-09-25 06:03:27 +0300

    nofollow links added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,45 @@
 import * as React from 'react'
+import { useRouter } from 'next/router'
 import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa'
 import { IoSunnyOutline, IoMoonSharp } from 'react-icons/io5'
 import * as config from 'lib/config'
+import Script from 'next/script'
 
 import styles from './styles.module.css'
 
-// TODO: merge the data and icons from PageSocial with the social links in Footer
-
+export default function Home() {
+  function makeLinksNoFollow() {
+    const whitelist = [
+      'www.filizguvenlik.com.tr',
+      'livicom.net',
+      'livicomturkiye.com',
+      'www.cbsofyalioglu.com',
+      'alarmsistemleri.org'
+    ]
+    if (window.location.pathname.includes('firma')) {
+      const allAnchors = document.querySelectorAll('a')
+      for (const a of allAnchors.values()) {
+        if (!whitelist.includes(a.host)) {
+          a.rel = 'nofollow noopener'
+        }
+      }
+    }
+  }
+  React.useEffect(() => {
+    makeLinksNoFollow()
+  }, [])
+  return (
+    <Script id='show-banner' strategy='lazyOnload' afterInteractive>
+      {}
+    </Script>
+  )
+}
 export const Footer: React.FC<{
   isDarkMode: boolean
   toggleDarkMode: () => void
 }> = ({ isDarkMode, toggleDarkMode }) => {
+  //const router = useRouter()
+  //console.log('\n\n', router.pathname)
   const [hasMounted, setHasMounted] = React.useState(false)
   const toggleDarkModeCb = React.useCallback(
     (e) => {
@@ -24,11 +53,13 @@ export const Footer: React.FC<{
     setHasMounted(true)
   }, [])
 
-
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright}>
-        Copyright 2021. <a href="https://www.cbsofyalioglu.com/" rel="me">{config.author}</a>
+        Copyright 2021.{' '}
+        <a href='https://www.cbsofyalioglu.com/' rel='me'>
+          {config.author}
+        </a>
       </div>
 
       {hasMounted ? (
@@ -80,6 +111,7 @@ export const Footer: React.FC<{
           </a>
         )}
       </div>
+      <Home />
     </footer>
   )
 }
